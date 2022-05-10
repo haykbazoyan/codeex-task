16166d7f04480db834ba2ddf860b2db36be7cf96
Hayk Bazoyan <bazoyan13@gmail.com>
2022-05-10 06:11:27 +0400

    changed dataAPI URL

--- src/restaurants/restaurants.js
+++ src/restaurants/restaurants.js
@@ -1,20 +1,16 @@
 import React from "react";
 
 export function Restaurants() {
-  //   const asd = new Request("/api/data", {
+  //   const asd = new Request("http://localhost:3000/api/data", {
   //     mode: "no-cors",
   //   });
 
-  const dataAPI = fetch("https://jsonplaceholder.typicode.com/users", {
-    mode: "no-cors",
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Credentials": true,
-    },
-  })
+  const dataAPI = fetch("http://localhost:3000/api/data")
     .then((response) => response.json())
     .then((json) => json)
     .catch((err) => console.log(err.message));
+
+  console.log("data:: ", dataAPI);
 
   //   return (
   //     <div className="list-container">
